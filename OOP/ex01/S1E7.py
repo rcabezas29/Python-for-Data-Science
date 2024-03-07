41bd874feadd3c5b2adf23f35ace369cd38af340
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2024-03-07 19:08:33 +0100

    Starting OOP

---
+++ OOP/ex01/S1E7.py
@@ -0,0 +1,13 @@
+from S1E9 import Character
+
+class Baratheon(Character):
+    """
+    Baratheon class
+    """
+class Lannister(Character):
+    """
+    Lannister class
+    """
+    # decorator
+    def create_lannister():
+        """"""
---
+++ OOP/ex01/S1E9.py
@@ -0,0 +1,44 @@
+from abc import ABC, abstractmethod
+
+
+class   Character(ABC):
+    """
+    Absatract class for every kind of GOT character
+    """
+
+    @abstractmethod
+    def __init__(self, first_name: str, is_alive=True):
+        """
+        Character class constructor.
+        Instantiates all characters from which this class is
+        inheriting.
+        """
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+ 
+class Stark(Character):
+    """
+    Stark class - The winter is coming.
+    Inherits from Character.
+    """
+
+    def __init__(self, first_name=None, is_alive=True):
+        """
+        Stark constructor that sets their house words and family name
+        """
+        super().__init__(first_name=first_name, is_alive=is_alive)
+        self.family_name = "Stark"
+        self.house_words = "Winter is Coming"
+        
+    def print_house_words(self):
+        """
+        Prints Starks house words
+        """
+        print('Winter is coming')
+
+    def die(self):
+        """
+        Sets alive attribute to False.
+        """
+        self.is_alive = False
