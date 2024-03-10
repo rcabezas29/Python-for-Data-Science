df98ddd790da6579a1cd95f181a7f0e798e94eeb
Raul Cabezas gonzalez <raul.29cg@gmail.com>
2024-03-10 09:08:24 +0100

    Finished - pending review

--- Dod/ex03/new_student.py
+++ Dod/ex03/new_student.py
@@ -2,14 +2,17 @@ import random
 import string
 from dataclasses import dataclass, field
 
+
 def generate_id() -> str:
-    return "".join(random.choices(string.ascii_lowercase, k = 15))
+    """Generates a random id"""
+    return "".join(random.choices(string.ascii_lowercase, k=15))
+
 
 def create_login(name: str, surname: str) -> str:
     """
     Creates a login based on name and surname
     """
-    return name[0] + surname[:7]
+    return (name[0] + surname[:7]).lower()
 
 
 @dataclass
@@ -19,6 +22,13 @@ class Student:
     """
     name: str
     surname: str
-    active: bool = True
-    id: str = field(default_factory=generate_id)
-    login: str = field(default_factory="")
+    active: bool = field(default=True)
+    id: str = field(default_factory=generate_id, init=False)
+    login: str = field(init=False)
+
+    def __post_init__(self):
+        """
+        Creates the login after the instantiation of the class
+        as name and surname are not generated previously
+        """
+        self.login = create_login(self.name, self.surname)
